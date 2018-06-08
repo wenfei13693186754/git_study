7709573c18584593116fd38c6a9e45a7fe1407f4
xiayufei <852283049@qq.com>
2018-06-08 10:05:03 +0800

    add raw

--- src/main/java/GitLabDemo.java
+++ src/main/java/GitLabDemo.java
@@ -10,5 +10,6 @@ public class GitLabDemo {
         System.out.println("gitlab学习使用1");
         System.out.println("gitlab学习使用1");
         System.out.println("gitlab学习使用1");
+        System.out.println("gitlab学习使用1");
     }
 }
