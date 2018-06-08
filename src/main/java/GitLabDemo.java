443d390442d1a48a337a8d52d07a556c7b5d1100
xiayufei <852283049@qq.com>
2018-06-08 09:41:59 +0800

    Update GitLabDemo.java

--- src/main/java/GitLabDemo.java
+++ src/main/java/GitLabDemo.java
@@ -7,5 +7,6 @@ public class GitLabDemo {
     public static void main(String[] args) {
         System.out.println("gitlab学习使用1");
         System.out.println("gitlab学习使用1");
+        System.out.println("gitlab学习使用1");
     }
 }
