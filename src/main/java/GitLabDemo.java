755f4632eab1e10156e2de197ae79eea2e58e1c3
xyf <852283049@qq.com>
2018-06-08 10:05:24 +0800

    新增GitLabDemo类

---
+++ src/main/java/GitLabDemo.java
@@ -0,0 +1,10 @@
+/**
+ * @author xyf
+ * @Data Created in 10:03 2018/6/8
+ * @Descriptions
+ */
+public class GitLabDemo {
+    public static void main(String[] args) {
+        System.out.println("gitlab学习使用");
+    }
+}
